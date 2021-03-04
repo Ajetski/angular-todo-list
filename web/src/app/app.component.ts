e78422a20e891664a1a66250e259e536e947baa7
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-03-03 23:07:44 -0500

    add alerts to handle errors

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -10,18 +10,42 @@ import { TaskService } from './services/task.service';
 export class AppComponent implements OnInit {
 	list: Task[] = [];
 
+	alert: {
+		show: boolean,
+		msg: string,
+		type: 'success' | 'danger'
+	} = {
+		show: false,
+		msg: '',
+		type: 'success'
+	};
+
 	constructor(public taskService: TaskService) {
 
 	}
 
-	ngOnInit() {
-		this.taskService.getTasks().subscribe(
-			res => {
-				this.list = res.map(({'_id': id, text, completed}) => ({
-					id, text, completed
-				}))
-			}
-		)
+	handleError(err) {
+		this.alert.msg = err.message ? err.message : err;
+		this.alert.type = 'danger';
+		this.alert.show = true;
+	}
+
+	handleSuccess(msg: string) {
+		// this.alert.msg = msg;
+		// this.alert.type = 'success';
+		// this.alert.show = true;
+	}
+
+	async ngOnInit() {
+		try {
+			const res = await this.taskService.getTasks().toPromise();
+			this.list = res.map(({'_id': id, text, completed}) => ({
+				id, text, completed
+			}));
+			this.handleSuccess('fetched tasks');
+		} catch (err) {
+			this.handleError(err);
+		}
 	}
 
 	async onAddItem(input: HTMLInputElement) {
@@ -35,9 +59,10 @@ export class AppComponent implements OnInit {
 				this.list.push({id, text, completed});
 				input.value = '';
 				input.focus();
+				this.handleSuccess('added task');
 			}
 		} catch (err) {
-
+			this.handleError(err);
 		}
 	}
 
@@ -45,8 +70,9 @@ export class AppComponent implements OnInit {
 		try {
 			await this.taskService.deleteTask(this.list[idx]).toPromise();
 			this.list = this.list.filter((el: Task, i: number) => i !== idx);
+			this.handleSuccess('deleted task');
 		} catch (err) {
-
+			this.handleError(err);
 		}
 	}
 
@@ -54,8 +80,9 @@ export class AppComponent implements OnInit {
 		try {
 			await this.taskService.deleteCompletedTasks().toPromise();
 			this.list = this.list.filter((el: Task) => !el.completed);
+			this.handleSuccess('deleted tasks');
 		} catch (err) {
-
+			this.handleError(err);
 		}
 	}
 }
